f34af9cbc17909fb93452673011a7f325f7948e8
Mike Pitre <michaelepitre@gmail.com>
2015-06-04 15:42:26 -0500

    added create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,8 @@ class MessagesController < ApplicationController
     render json: Message.all
   end
 
+  def create
+    render json: Message.create(username: params.fetch(:username), text: params.fetch(:text))
+  end
+
 end
