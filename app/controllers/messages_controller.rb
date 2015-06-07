cd644ced81cf131b7bf22102720607507ef8232a
Mike Pitre <michaelepitre@gmail.com>
2015-06-07 15:34:21 -0500

    added chatbot method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,7 @@ class MessagesController < ApplicationController
 
   def create
     render json: Message.create(username: params.fetch(:username), text: Swearjar.default.censor(params.fetch(:text)), chatroom: params.fetch(:chatroom))
+    bot_response
   end
 
   def leaderboard
@@ -23,7 +24,6 @@ class MessagesController < ApplicationController
     render json: recent
   end
 
-
   def active_chatrooms
     chatrooms = Message.all.group(:chatroom).count.sort_by { |chatroom, count| count }.last(10).reverse
     chatrooms = Hash[chatrooms]
@@ -32,6 +32,21 @@ class MessagesController < ApplicationController
 
   def profile
     render json: Message.where(username: params[:username])
+  end
+
+  def bot_response
+    if params[:chatroom] == "4"
+      case params.fetch(:text)
+      when "do you love me?"
+        Message.create(username: "Chatbot", text: "Yes, let's make cyborg babies!", chatroom: 4)
+      when "hi"
+        Message.create(username: "Chatbot", text: "Help! I'm trapped in the computer!", chatroom: 4)
+      when "how are you?"
+        Message.create(username: "Chatbot", text: "Same as always, I'm a chatbot", chatroom: 4)
+      else
+        Message.create(username: "Chatbot", text: "I'm a stupid chatbot. Try 'Hi', 'How are you?' or 'do you love me? ;)", chatroom: 4)
+      end
+    end
   end
 
 end
